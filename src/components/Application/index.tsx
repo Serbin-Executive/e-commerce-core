103c12b5478b95c9c7969330a1e3e5a336104051
Andrew Serbin <serbinadrian@outlook.com>
2025-02-28 17:13:10 +0300

    [EC-0] Initial commit

---
+++ src/components/Application/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react'
+import './styles.css'
+
+const Application = (): ReactElement  => {
+
+  return (
+    <div>Application</div>
+  )
+}
+
+export default Application;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import Application from '@components/Application'
+import ReactDOM, { Root } from 'react-dom/client'
+
+const RootHTMLElement: HTMLElement | null = document.getElementById('root');
+
+if (!RootHTMLElement) {
+  throw new Error("Cannot acquire div#root to start the Application");
+}
+
+const ReactRoot: Root = ReactDOM.createRoot(RootHTMLElement);
+
+ReactRoot.render(<Application />);
